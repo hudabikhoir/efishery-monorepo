a862ad5fad0a2707180bfa2a7480e2faed88c8f2
hudabikhoir <nurhuda@alterra.id>
2021-06-06 01:42:13 +0700

    feat(web-server): response commodities report
    create response commodity reports to show in rest api

---
+++ fetch-app/api/v1/commodity/response/get_commodity_report.go
@@ -0,0 +1,44 @@
+package response
+
+import (
+	"efishery/business/commodity"
+)
+
+type GetCommodityReportResponse struct {
+	Province string  `json:"province"`
+	Min      float64 `json:"min"`
+	Max      float64 `json:"max"`
+	Median   int     `json:"median"`
+	Average  float64 `json:"average"`
+}
+
+//GetCommoditieResponse Get commoditie by tag response payload
+type GetCommoditiesReportResponse struct {
+	CommoditiesReport []*GetCommodityReportResponse `json:"commodities_report"`
+}
+
+//NewGetCommodityReportResponse construct GetCommodityReportResponse
+func NewGetCommodityReportResponse(commodity commodity.CommodityReport) *GetCommodityReportResponse {
+	var commodityResponse GetCommodityReportResponse
+	commodityResponse.Province = commodity.Province
+	commodityResponse.Min = commodity.Min
+	commodityResponse.Max = commodity.Max
+	commodityResponse.Median = commodity.Median
+	commodityResponse.Average = commodity.Average
+
+	return &commodityResponse
+}
+
+//NewGetCommoditieResponse construct GetCommoditieResponse
+func NewGetCommoditiesReportResponse(commodities []commodity.CommodityReport) *GetCommoditiesReportResponse {
+	var commoditiesResponses []*GetCommodityReportResponse
+	commoditiesResponses = make([]*GetCommodityReportResponse, 0)
+
+	for _, commodities := range commodities {
+		commoditiesResponses = append(commoditiesResponses, NewGetCommodityReportResponse(commodities))
+	}
+
+	return &GetCommoditiesReportResponse{
+		commoditiesResponses,
+	}
+}
